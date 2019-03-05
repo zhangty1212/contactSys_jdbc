326e7de94325e8454133edd1e31acc3f90d81981
dengxing <374294495@qq.com>
2019-03-05 08:43:37 +0800

    编码问题

--- src/gz/itcast/contactSys_web/servlet/LoginServlet.java
+++ src/gz/itcast/contactSys_web/servlet/LoginServlet.java
@@ -1,6 +1,7 @@
 package gz.itcast.contactSys_web.servlet;
 
 import java.io.IOException;
+import java.net.URLEncoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -56,19 +57,30 @@ public class LoginServlet extends HttpServlet {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.setContentType("text/html;charset=UTF-8");
-		//request.setCharacterEncoding("utf-8");
-		//response.setHeader("Content-type", "text/html; charset=UTF-8"); 
+		//request.setCharacterEncoding("UTF-8");
+		//response.setHeader("Content-type", "text/html; charset=UTF-8");
+		//response.setHeader("Content-Type", "text/html;charset=UTF-8");
+		// 设置浏览器默认打开的时候采用的字符集编码
+		System.out.println(request.getParameter("username"));
 		response.getWriter().append("Served at: ").append(request.getContextPath() + "<br/>");
 		response.getWriter().write(getServletName() + "<br/>");
 		response.getWriter().write(request.getMethod() + "<br/>");
 		response.getWriter().write("后台返回用户名" + "<br/>");
 		response.getWriter().write("-----------------------------------------" + "<br/>");
-		response.getWriter().write(request.getParameter("usrname"));
+
+		String username = request.getParameter("username");
+		System.out.println(username);
+		String str = new String(username.getBytes("ISO-8859-1"),"utf-8");
+		response.getWriter().write(str);
 		response.getWriter().write("<br/>");
 		response.getWriter().write("-----------------------------------------" + "<br/>");
 		response.getWriter().write("后台返回密码" + "<br/>");
 		response.getWriter().write("-----------------------------------------" + "<br/>");
-		response.getWriter().write(request.getParameter("passward"));
+		String password = request.getParameter("password");
+		System.out.println(password);
+		String str1 = new String(username.getBytes("ISO-8859-1"),"utf-8");
+		response.getWriter().write(str1);
+
 	}
 
 }
